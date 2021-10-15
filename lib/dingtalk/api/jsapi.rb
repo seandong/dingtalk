38b6317165404c4c019b68c522e766229b73567b
seandong <sindon@gmail.com>
2021-10-15 15:45:47 +0800

    FIX: bug

--- lib/dingtalk/api/jsapi.rb
+++ lib/dingtalk/api/jsapi.rb
@@ -3,7 +3,7 @@ module Dingtalk
     module Jsapi
       def signature_package(url)
         noncestr = SecureRandom.hex(4)
-        timestamp = Time.now.to_i.to_s
+        timestamp = Time.now.to_i
         signature = Digest::SHA1.hexdigest(
           "jsapi_ticket=#{jsapi_ticket}&noncestr=#{noncestr}&timestamp=#{timestamp}&url=#{url}"
         )
